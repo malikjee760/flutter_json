49c8ab4a8647b2a223c6d65af1029da95ca9a524
malikjee760 <85275592+malikjee760@users.noreply.github.com>
2021-08-11 15:46:35 +0500

    third commit

---
+++ lib/data.dart
@@ -0,0 +1,6 @@
+class data {
+  int id, userId;
+  String title, body;
+
+  data(this.id, this.userId, this.title, this.body);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_jason/data.dart';
 import 'model/post.dart';
+import 'data.dart';
 
 void main() async {
   List myData = await getjsonData();
-  print(myData);
+  List data1 = [];
+  for (int i = 0; i < myData.length; i++) {
+    int id = myData[i]['id'];
+    int userId = myData[i]['userId'];
+    String title = myData[i]['title'];
+    String body = myData[i]['body'];
+
+    data1.add(new data(id, userId, title, body));
+  }
+  //print(data1);
 
   runApp(new MaterialApp(
     title: "API",
@@ -12,8 +22,20 @@ void main() async {
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
       ),
-      //body: ,
+      body: Center(
+        child: ListView.builder(
+            itemCount: myData.length,
+            itemBuilder: (BuildContext context, int position) {
+              return new Column(
+                children: [
+                  Text(myData[position]['id'].toString()),
+                  Text(myData[position]['userId'].toString()),
+                  Text(myData[position]['title']),
+                  Text(myData[position]['body']),
+                ],
+              );
+            }),
+      ),
     ),
   ));
 }
-
